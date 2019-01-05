104cd74cb66cd4d8f6e4cb94e780588069739e06
thiagoleitedev <thiagoleitebr@gmail.com>
2019-01-05 15:50:09 -0300

    Add basic configuration for apollo client

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { RestLink } from 'apollo-link-rest';
+
 import Routes from './Routes';
 import './style.css';
 
+const restLink = new RestLink({
+  uri: 'http://jsonplaceholder.typicode.com',
+});
+
+const client = new ApolloClient({
+  link: restLink,
+  cache: new InMemoryCache(),
+});
+
+const App: React.SFC<{}> = () => (
+  <ApolloProvider client={client}>
     <Routes />
+  </ApolloProvider>
+);
 
 export default hot(module)(App);
