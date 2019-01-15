90caaab3bf558dd37ee29534bafcdb3955c717d2
thiagoleitedev <thiagoleitebr@gmail.com>
2019-01-15 20:56:02 -0300

    add user get router

--- packages/server/src/modules/user/routes/UserGet.ts
+++ packages/server/src/modules/user/routes/UserGet.ts
@@ -1,11 +1,17 @@
-const userGet = async ctx => {
+import { ApiContext } from '../../../TypeDefinition';
+
+const userGet = async (ctx: ApiContext) => {
   ctx.status = 200;
   ctx.body = {};
 };
 
-const usersGet = async ctx => {
-  ctx.status = 200;
-  ctx.body = {};
+const usersGet = async (ctx: ApiContext) => {
+  try {
+    ctx.status = 200;
+    ctx.body = await UserLoader.loadUsers(ctx, ctx.args);
+  } catch (err) {
+    ctx.throw(404, err);
+  }
 };
 
 export { userGet, usersGet };
