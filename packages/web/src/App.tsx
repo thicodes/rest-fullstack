74b7e809e3392023250be86af00350f513eade83
thiagoleitedev <thiagoleitebr@gmail.com>
2019-01-15 21:00:11 -0300

    add authentication with localstorage and context

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,17 +3,30 @@ import { hot } from 'react-hot-loader';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
+import { setContext } from 'apollo-link-context';
 import { RestLink } from 'apollo-link-rest';
 
 import Routes from './Routes';
 import './style.css';
 
+// Setup auth and store token
+const authRestLink = setContext((_, { headers }) => {
+  const token = window.localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token,
+    },
+  };
+});
+
+// Setup the domain of your endpoint here
 const restLink = new RestLink({
   uri: 'http://jsonplaceholder.typicode.com',
 });
 
 const client = new ApolloClient({
-  link: restLink,
+  link: authRestLink.concat(restLink),
   cache: new InMemoryCache(),
 });
 
