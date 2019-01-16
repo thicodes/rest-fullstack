abf4981dedd57a78c1d5ad172e67d21f6e7c79e2
thiagoleitedev <thiagoleitebr@gmail.com>
2019-01-15 21:04:28 -0300

    add query to get users and list

--- packages/web/src/components/UserList.tsx
+++ packages/web/src/components/UserList.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { graphql, Query } from 'react-apollo';
 
-const UserList = () => <p>UserList</p>;
+import { GET_USERS } from '../apollo/queries';
+
+const UserList = () => (
+  <Query query={GET_USERS}>
+    {({ loading, error, data }) => {
+      if (loading) {
+        return <p>Loading...</p>;
+      }
+      if (error) return <p>Error</p>;
+      return (
+        <div>
+          {data.users.map((data, index) => (
+            <React.Fragment key={data.id}>
+              <div>
+                <b>name:</b> {data.name}
+              </div>
+              <div>
+                <b>username:</b> {data.name}
+              </div>
+            </React.Fragment>
+          ))}
+        </div>
+      );
+    }}
+  </Query>
+);
 
 export default UserList;
