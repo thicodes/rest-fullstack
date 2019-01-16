97c291459863e5f21b9b54055f994227a384f5eb
thiagoleitedev <thiagoleitebr@gmail.com>
2019-01-15 21:07:03 -0300

    add user create component

--- packages/web/src/components/UserCreate.tsx
+++ packages/web/src/components/UserCreate.tsx
@@ -1,3 +1,43 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import { withRouter } from 'react-router-dom';
+
+import { CREATE_USER } from '../apollo/mutations';
+import { GET_USERS } from '../apollo/queries';
+
+interface State {
+  name: string;
+  username: string;
+}
+
+class UserCreate extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      name: '',
+      username: '',
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e, mutation) => {
+    e.preventDefault();
+
+    mutation({
+      variables: {
+        input: {
+          ...this.state,
+        },
+      },
+    });
+
+    this.props.history.push('/');
+  };
+
   updateCache = (cache: object, { data: { createUser } }: object) => {
     const { users } = cache.readQuery({ query: GET_USERS });
 
@@ -7,6 +47,11 @@
     });
   };
 
+  render() {
+    const { name, username } = this.state;
+
+    return (
+      <div>
         <Mutation mutation={CREATE_USER} update={this.updateCache}>
           {mutation => (
             <form onSubmit={e => this.handleSubmit(e, mutation)}>
@@ -28,5 +73,9 @@
             </form>
           )}
         </Mutation>
+      </div>
+    );
+  }
+}
 
-export default UserCreate;
+export default withRouter(UserCreate);
